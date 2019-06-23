914e6109c49feb6af38df1f0e2ac968cf2f21aab
Standley Eugene <estand25@gmail.com>
2019-06-22 22:35:13 -0400

    Replace all local functional calls with map dispatch actions

--- src/components/CFrame.js
+++ src/components/CFrame.js
@@ -7,32 +7,11 @@ import {
     numberPicked, 
     equalPicked 
 } from '../actions'
-
 import '../App.css'
 import DisplayButtons from './DisplayButtons';
 import ComplexDisplayButton from './ComplexDisplayButton';
 
 class CFrame extends React.Component {
-    onEqualClick(){
-        this.props.equalPicked();
-    }
-
-    onOperationClick(Ope){
-        this.props.operationPicked(Ope);
-    }
-
-    onNumberClick(n){
-        this.props.numberPicked(n);
-    }
-
-    clearOnClick(){
-        this.props.clearPicked();
-    }
-
-    emptyOnClick(){
-        this.props.emptyPicked();
-    }
-
     render() {
         return (
             <div className="Frame-Main">
@@ -48,88 +27,88 @@ class CFrame extends React.Component {
                     <DisplayButtons
                         sty={"Button-Clear"}
                         input={"c"}
-                        onClick={() => this.clearOnClick()}
+                        onClick={() => this.props.clearPicked()}
                     />
                     <DisplayButtons
                         sty={"Button-Clear"}
                         input={"e"}
-                        onClick={() => this.emptyOnClick()}
+                        onClick={() => this.props.emptyPicked()}
                     />
                     <DisplayButtons
                         sty={"Button-Operation"}
                         input={"*"}
-                        onClick={() => this.onOperationClick('m')}
+                        onClick={() =>this.props.operationPicked('m')}
                     />
                     <DisplayButtons
                         sty={"Button-Operation"}
                         input={"\xF7"}
-                        onClick={() => this.onOperationClick('d')}
+                        onClick={() => this.props.operationPicked('d')}
                     />
                 </div>
                 <div className="row">
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={7}
-                        onClick={() => this.onNumberClick(7)}
+                        onClick={() => this.props.numberPicked(7)}
                     />
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={8}
-                        onClick={() => this.onNumberClick(8)}
+                        onClick={() => this.props.numberPicked(8)}
                     />
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={9}
-                        onClick={() => this.onNumberClick(9)}
+                        onClick={() => this.props.numberPicked(9)}
                     />
                     <DisplayButtons
                         sty={"Button-Operation"}
                         input={"\u2212"}
-                        onClick={() => this.onOperationClick('s')}
+                        onClick={() => this.props.operationPicked('s')}
                     />
                 </div>
                 <div className="row">
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={4}
-                        onClick={() => this.onNumberClick(4)}
+                        onClick={() => this.props.numberPicked(4)}
                     />
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={5}
-                        onClick={() => this.onNumberClick(5)}
+                        onClick={() => this.props.numberPicked(5)}
                     />
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={6}
-                        onClick={() => this.onNumberClick(6)}
+                        onClick={() => this.props.numberPicked(6)}
                     />
                     <DisplayButtons
                         sty={"Button-Operation"}
                         input={"+"}
-                        onClick={() => this.onOperationClick('a')}
+                        onClick={() => this.props.operationPicked('a')}
                     />
                 </div>
                 <div className="row">
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={1}
-                        onClick={() => this.onNumberClick(1)}
+                        onClick={() => this.props.numberPicked(1)}
                     />
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={2}
-                        onClick={() => this.onNumberClick(2)}
+                        onClick={() => this.props.numberPicked(2)}
                     />
                     <DisplayButtons
                         sty={"Button-Number"}
                         input={3}
-                        onClick={() => this.onNumberClick(3)}
+                        onClick={() => this.props.numberPicked(3)}
                     />
                     <DisplayButtons
                         sty={"Button-Operation"}
                         input={"="}
-                        onClick={() => this.onEqualClick()}
+                        onClick={() => this.props.equalPicked()}
                     />
                 </div>
             </div>
