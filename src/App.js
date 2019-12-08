806b7b08c656b1a240aedc960ffa92647a9dccc0
Standley Eugene <estand25@gmail.com>
2019-12-07 20:12:08 -0500

    Remove unneccessary js files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { calculator } from './reducers'
 import Display from './page/Display'
-// import './App.css';
 
 const store = createStore(calculator)
 
