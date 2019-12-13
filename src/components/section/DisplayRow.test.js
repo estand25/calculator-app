ee0a635d5c47d1828ff33be8590419312a181ba0
Standley Eugene <estand25@gmail.com>
2019-12-12 22:17:17 -0500

    Add one more DisplayRow test

--- src/components/section/DisplayRow.test.js
+++ src/components/section/DisplayRow.test.js
@@ -29,3 +29,27 @@ describe('', () => {
     })
 
 })
+
+
+describe('With output logs', () => {
+    const initialState = { 
+        layOut: '', 
+        clearStyle: 'Button-Clear',
+        emptyStyle: 'Button-Empty',
+        operStyle: 'Button-Operation',
+        numbStyle: 'Button-Number',
+    }
+    const mockStore = configureStore()
+    let store
+    
+    it('render without crashing', () => {
+        store = mockStore(initialState)
+        const displayRow = render(
+            <Provider store={store}>
+                <DisplayRow />
+            </Provider>
+        )
+        expect(displayRow).toBeTruthy()
+    })
+
+})
